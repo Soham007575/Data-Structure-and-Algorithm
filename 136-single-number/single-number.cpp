e189814ed40108f15fd01a483c1eda7fce6e47ff
Soham <sohamantre007@gmail.com>
2026-09-25 13:57:03 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (83.74%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -3,7 +3,7 @@ public:
     int singleNumber(vector<int>& nums) {
         int ans=0;
         for(int val : nums){
-            ans ^= val; 
+            ans ^= val;  // bitwise operator XOR use in it
         }
         return ans;
         
